5bb26da247e5b35ffcb7c7e84f0036c4758d8940
Krzysztof Balka <krzysztof.balka@gmail.com>
2016-10-27 00:02:20 +0200

    Collecting results from channel in goroutine

--- parallel/parallel.go
+++ parallel/parallel.go
@@ -1,38 +1,58 @@
 package main
 
 import (
-//    "bufio"
-    "fmt"
-    "time"
-//    "math"
-//    "os"
-//    "runtime"
+	//    "bufio"
+	"fmt"
+	"time"
+	//    "math"
+	//    "os"
+	//    "runtime"
 )
 
-func calculate(input chan int) {
-        go func() {
-          liczba := <- input
-	  fmt.Printf("Starting calculation for %d ", liczba)
-          for i:=2;i < liczba; i++ {
-            if liczba % i ==0 {
-             fmt.Printf("Not prime, divided by %d\n", i)
-             return
-            }
-          }
-          fmt.Printf(" XXXXXXXXXXXXXXXXXXXXXX Prime\n")
-        }()
+var min int
+var max int
+
+func calculate(input chan int, output chan int) {
+	go func() {
+		liczba := <-input
+		for i := 2; i < liczba; i++ {
+			if liczba%i == 0 {
+				output <- 0
+				return
+			}
+		}
+		time.Sleep(5000)
+		output <- liczba
+	}()
 }
 
+func collect(output chan int) {
+	go func() {
+		for i := min; i < max; i++ {
+			liczba := <-output
+			fmt.Printf("[%d] collected: %d\n", i, liczba)
+		}
+	}()
+}
 
 func main() {
- fmt.Printf("Hello world\n")
- inputs := make(chan int)
- for i:=500;i < 600; i++ {
-   calculate(inputs)
- }
- for i:=500;i < 600; i++ {
-   inputs <- i*i - 1
- }
- fmt.Printf("Created 100 inputs\n")
- time.Sleep(3000*time.Millisecond)
+	fmt.Printf("Parallel prime number calculator\n")
+	min, max = 500, 600
+	inputs := make(chan int)
+	output := make(chan int)
+	// create goroutine for result collection
+	collect(output)
+
+	// create goroutines for calculation
+	for i := min; i < max; i++ {
+		calculate(inputs, output)
+	}
+
+	// distribute numbers through channels
+	for i := min; i < max; i++ {
+		inputs <- i
+	}
+
+	fmt.Printf("Created %d inputs\n", max-min)
+	time.Sleep(3000 * time.Millisecond)
 }
